c6c833fb7c228218a31c948f16890be0dc1f9414
RanaVikramSinha <110714857+RanaVikramSinha@users.noreply.github.com>
2022-08-14 21:24:17 +0530

    Add files via upload

---
+++ Assignment 07/9.c
@@ -0,0 +1,37 @@
+/* 9. Write a program to check whether a given number is an Armstrong number
+or not */ 
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num,temp,rem,sum=0,count=0;
+  
+    printf("Enter a number\n");
+    scanf("%d",&num);
+    
+    temp=num;
+    
+    while(temp!=0)    
+    {
+        temp=temp / 10;
+        count++;
+    }
+    
+    temp=num;
+    
+    while(temp>0)     
+    {
+        rem=temp%10;
+        sum=sum+pow(rem, count);
+        temp=temp / 10;
+    }
+    
+    if(sum==num)
+    printf ("%d is an Armstrong number\n",num);
+    else
+    printf ("%d is not an Armstrong number\n",num);
+
+    return 0;
+}
